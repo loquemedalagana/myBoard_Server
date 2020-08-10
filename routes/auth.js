51ca6306ac6187b8224c28157974daf6a6fa2069
loquemedalagana <nadamasquetu012@gmail.com>
2020-08-10 13:56:10 +0900

    add error handling

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,11 @@ router.get('/', (req, res) => {
 });
 
 router.post('/signup', (req, res) => {
-    console.log(req.body); //express 4.16+ 에는 body가 내장
+    const {name, email, password} = req.body;
+    if(!name || !email || !password){
+        res.status(422).json({error: "please add all the fields except your SNS links"});
+    }
+    else res.json({message: "sucessfully posted"});
 });
 
 module.exports = router;
