bf7eabf6d0bf3f155e4fda10e66dcb38306637f9
loquemedalagana <nadamasquetu012@gmail.com>
2020-08-10 11:34:59 +0900

    middleware practice

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const app = express();
 
 const PORT = 5000;
 
+const customMiddleware = (req, res, next) => {
+    console.log("middleware executed!");
+    next(); //다음 미들웨어 수행을 위해 사용
+}
+
+//app.use(customMiddleware);
+
 app.get('/', (req, res) => {
+    console.log("hello world");
     res.send("hello world");
 })
+
+app.get('/about', customMiddleware, (req, res) => {
+    console.log("about");
+    res.send("about");
+});
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
