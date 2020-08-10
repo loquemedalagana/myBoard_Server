86faca204b2ebcb35f74ef3f6056941200d39383
loquemedalagana <nadamasquetu012@gmail.com>
2020-08-10 17:15:53 +0900

    add sign up

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
 router.get('/', (req, res) => {
     res.send("express is running");
 });
@@ -8,9 +11,34 @@ router.get('/', (req, res) => {
 router.post('/signup', (req, res) => {
     const {name, email, password} = req.body;
     if(!name || !email || !password){
-        res.status(422).json({error: "please add all the fields except your SNS links"});
+        return res.status(422).json({error: "please add all the fields except your SNS links"});
     }
-    else res.json({message: "sucessfully posted"});
+    //else res.json({message: "sucessfully posted"}); 나중에 프론트에 넘길 때 제이슨 이용
+    User.findOne({email : email}) //promise (producer)
+    .then( (savedUser) => {//success (consumer) 중복된 회원이 있을 때
+        if(savedUser){
+            return res.status(422).json({error: "이미 가입된 이메일입니다."});
+        }
+        else{
+            const user = new User({ //new user
+                name,
+                email,
+                password
+            });
+
+            user.save()
+            .then(user => {
+                res.json({message : "saved successfully"});
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+    })
+    .catch(err => { //error
+        console.log(err);
+    }) 
+    .finally(); //끝나고 무조건 실행
 });
 
 module.exports = router;
