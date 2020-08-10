914dc3a3bbf42a484135123c2a2e762d1a6ae198
loquemedalagana <nadamasquetu012@gmail.com>
2020-08-10 11:58:04 +0900

    connect to MONGODB

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
-const app = express();
+const mongoose = require("mongoose");
+const {MONGOURL} = require('./keys');
 
+const app = express();
 const PORT = 5000;
+
+mongoose.connect(MONGOURL,  { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on('connected', () => {
+    console.log("connected to mongo");
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log("error", err);
+});
 
 const customMiddleware = (req, res, next) => {
     console.log("middleware executed!");
